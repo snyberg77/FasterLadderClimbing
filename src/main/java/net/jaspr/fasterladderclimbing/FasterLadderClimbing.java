74e70eb1bd87713591d957a8456b615091b87769
Alek Rollyson <alek.rollyson@gmail.com>
2020-11-16 14:55:32 -0500

    Fix "incompatible modded server" messages

--- src/main/java/net/jaspr/fasterladderclimbing/FasterLadderClimbing.java
+++ src/main/java/net/jaspr/fasterladderclimbing/FasterLadderClimbing.java
@@ -23,8 +23,11 @@ import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -35,6 +38,9 @@ public class FasterLadderClimbing {
 	public static final Logger LOGGER = LogManager.getLogger(FasterLadderClimbing.MOD_ID);
 
 	public FasterLadderClimbing() {
+		//Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+
 		// Load Config
 		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, FasterLadderClimbingConfig.CONFIG_SPEC);
 
